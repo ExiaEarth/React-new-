52f5d2ec700c23a4b59c5914e0f0157d4926f78c
Marclemer <marc-lemer@hotmail.com>
2022-08-31 10:38:37 +0200

    pause10h30(mise en place du scss/search-var/searchBT)

--- movie-react/src/containers/Search-bar/search-bar.jsx
+++ movie-react/src/containers/Search-bar/search-bar.jsx
@@ -1,8 +1,18 @@
-const SearchBar=()=> {
+import { useState } from "react";
+import style from "./search-bar.module.scss";
+
+const SearchBar=(props)=> {
+    const {placeholder}=props
+    const {onSearch}=props
+
+    const [input,setInput]=useState('')
+    const handleClick=()=>{ console.log(input);}
+    onSearch(input)
+
     return(
-        <div>
-        <input type="text" />
-        <button>Search</button>
+        <div className={style.container}>
+        <input type="text" value={input} onChange={e=>setInput(e.target.value)} placeholder={placeholder} />
+        <button onClick={handleClick}>Search</button>
         </div>
     );
 }
--- movie-react/src/pages/search/search.jsx
+++ movie-react/src/pages/search/search.jsx
@@ -1,9 +1,15 @@
-import SearchBar from "../../containers/Search-bar/search-bar";
+import axios from "axios"; 
+import { useState } from "react";
+import SearchBar from "../../containers/search-bar/search-bar";
 
 const Search=()=> {
+    const[movies,setMovies]=useState([])
+    // const searchFilm=(movieName)=>{console.log(movieName);}
+    const searchFilm=(movieName)=>{axios.get()}
+
     return(
         <div>
-            <SearchBar/>
+            <SearchBar placeholder="movie name" onSearch={searchFilm}/>
         </div>
     );
 }
